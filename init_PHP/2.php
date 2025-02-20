6b4370f0050512759fdfb4cd6512827c57424a79
OCHOASV <34662161+OCHOASV@users.noreply.github.com>
2025-02-20 20:49:44 +0100

    Updated

--- init_PHP/2.php
+++ init_PHP/2.php
@@ -4,42 +4,65 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ejemplos 2</title>
+
+	<style>
+		.rojo{
+			background: #000;
+			color:red;
+			font-size: 2rem;
+		}
+		.azul{
+			color:blue;
+			cursor: pointer;
+		}
+		.teal{
+			color:teal;
+		}
+		.btn{
+			font-size: 0.9rem;
+			cursor: pointer;
+			background-color: #0F6ED0;
+			color: white;
+			border: 1px solid black;
+			border-radius: 5px;
+		}
+		.btn:hover{
+			color: #0F6ED0;
+			background-color: white;
+			border: 1px solid #0F6ED0;
+		}
+	</style>
 </head>
 <body>
 
-	<?php
-		$sex = 2;
-
-		if ($sex == 0) {
-			echo 'Eres una Chica!';
-		}
-		elseif ($sex == 1){
-			echo 'Eres una Chico!';
-		}
-		else{
-			echo 'Eres Otro!';
-		}
-	?>
-
-	<hr>
-
-	<form action="">
-		<input type="text" id="resp" name="resp" placeholder="Escribe un Color">
-		<br>
-		<br>
-		<input type="submit" value="Enviar">
+	<form action="" method="POST">
+		<input type="text" id="color" name="color" placeholder="Escribe un Color" autofocus>
+		<input type="submit" class="btn" value="Enviar">
 	</form>
 
 	<?php
-		// $r = $_GET["resp"];
-		// // echo $r;
+		$color = $_POST['color'];
 
-		// if ($r == 'Rojo') {
-		// 	echo 'Eres el Ganador!';
-		// }
-		// else{
-		// 	echo 'Intentalo de nuevo...';
-		// }
+		if ($color == 'rojo') {
+	?>
+		<span class="rojo">Eres color Rojo</span>
+	<?php
+		}
+		elseif ($color == 'azul'){
+	?>
+		<span class="azul">Eres color Azul</span>
+	<?php
+		}
+		elseif ($color == 'teal'){
+	?>
+		<span class="teal">Eres color Teal</span>
+	<?php
+		}
+		else{
+	?>
+		<span>Eres otro color...</span>
+	<?php
+		}
 	?>
 
 </body>
