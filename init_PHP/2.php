81c27d22fbd56e9bf59e6a63c8fd34d49a8cdc74
OCHOASV <34662161+OCHOASV@users.noreply.github.com>
2025-02-20 20:10:27 +0100

    Updated

--- init_PHP/2.php
+++ init_PHP/2.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Generos</title>
+	<title>Ejemplos 2</title>
 </head>
 <body>
 
@@ -19,6 +19,27 @@
 		else{
 			echo 'Eres Otro!';
 		}
+	?>
+
+	<hr>
+
+	<form action="">
+		<input type="text" id="resp" name="resp" placeholder="Escribe un Color">
+		<br>
+		<br>
+		<input type="submit" value="Enviar">
+	</form>
+
+	<?php
+		// $r = $_GET["resp"];
+		// // echo $r;
+
+		// if ($r == 'Rojo') {
+		// 	echo 'Eres el Ganador!';
+		// }
+		// else{
+		// 	echo 'Intentalo de nuevo...';
+		// }
 	?>
 
 </body>
