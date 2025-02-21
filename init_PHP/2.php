147e5448c19464776ac0ab2abb93f4d6fe1f3fa8
OCHOASV <34662161+OCHOASV@users.noreply.github.com>
2025-02-21 19:32:32 +0100

    Updated

--- init_PHP/2.php
+++ init_PHP/2.php
@@ -5,46 +5,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ejemplos 2</title>
 
-	<style>
-		.rojo{
-			background: #000;
-			color:red;
-			font-size: 2rem;
-		}
-		.azul{
-			color:blue;
-			cursor: pointer;
-		}
-		.teal{
-			color:teal;
-		}
-		.btn{
-			font-size: 0.9rem;
-			cursor: pointer;
-			background-color: #0F6ED0;
-			color: white;
-			border: 1px solid black;
-			border-radius: 5px;
-		}
-		.btn:hover{
-			color: #0F6ED0;
-			background-color: white;
-			border: 1px solid #0F6ED0;
-		}
-	</style>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-	<form action="" method="POST">
-		<input type="text" id="color" name="color" class="caja" placeholder="Escribe un Color" autofocus>
+	<form action="procesar.php" method="POST">
+		<input type="text" id="color" name="color" placeholder="Escribe un Color" autofocus>
 		<input type="submit" class="btn" value="Enviar">
 	</form>
-
-	<?php
-		$color = $_POST['color'];
-
-		echo 'Color => '.$color;
-	?>
 
 </body>
 </html>
